d1f7e618e464147a249d67c447515a68d7cac16b
Game110AM <Game110AM@605-110-STU06>
2017-03-07 09:24:33 -0700

    blah

---
+++ flying cat bananas/flying cat bananas/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	char input;
+	char(w);
+	char(e);
+	char(s);
+	char(t);
+	cout << "you have awoken,in a room with a mirror roughly forced into the west wall" << endl;
+	cout << "by the way,type the letters (e),(w),(t),(s) to go to certain directions." << endl;
+	
+		
+
+}
